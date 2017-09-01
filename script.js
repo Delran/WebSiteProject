4be05ba5d5d5518c52d409f0a084508b093d0f97
delran <delran@hotmail.fr>
2017-09-02 00:11:19 +0200

    JS MAKING

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
-function mouriesMap()
-{
-    var mouries = {lat: 43.69, lng: 4.87166666667};
-    var mapOptions = {center: mouries, zoom: 11, mapTypeId: google.maps.MapTypeId.HYBRID};
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var marker = new google.maps.Marker({position: mouries, map: map});
-}
+$(".navLink").click(function() {
+   $('html, body').animate({scrollTop:$($.attr(this, 'link')).offset().top}, 1000);
+   $('.navLink').update("VTFFF MONDE");
+ });
