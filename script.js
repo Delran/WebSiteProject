38dca2a094d799c54ac0765229a74815a2e92588
floalazais <flo.alazais@gmail.com>
2017-09-02 00:45:14 +0200

    ajout du smooth scroll sur le logo

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-$(".navLink").click(function() {
-   $('html, body').animate({scrollTop:$($.attr(this, 'link')).offset().top}, 1000);
-   $('.navLink').update("VTFFF MONDE");
- });
+$(".logo").click(function(){$('html, body').animate({scrollTop:$($.attr(this,'link')).offset().top},'slow');});
+$(".navLink").click(function(){$('html, body').animate({scrollTop:$($.attr(this,'link')).offset().top},'slow');});
